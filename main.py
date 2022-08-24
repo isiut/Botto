7405ca4eade1ba046b6a0ef665392f55675b28b4
Illia Siutkin <illiasiutkin07@gmail.com>
2022-08-23 19:09:11 -0500

    add presence

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import json
 import os
 
 f = open('config.json', "r")
-token = json.loads(f.read())  # config.json has the token as a string
+token = json.loads(f.read())  # config.json has the token as a string (in .gitignore)
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(">"))
 
 @bot.event
 async def on_ready():
     print("The bot is online.")
+    await bot.change_presence(activity=discord.Game("with the bot"))
 
 for f in os.listdir("./cogs"):
     if f.endswith(".py"):
